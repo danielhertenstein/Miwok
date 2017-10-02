e499f75cbee4dda8a53dbb9944978fa40e68a7af
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2017-10-02 15:43:09 -0400

    Adds sounds

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -9,18 +9,27 @@ public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
     private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private int mAudioResourceId = NO_AUDIO_PROVIDED;
 
     public static final int NO_IMAGE_PROVIDED = -1;
+    public static final int NO_AUDIO_PROVIDED = -1;
 
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
     }
 
 
@@ -33,6 +42,8 @@ public class Word {
     }
 
     public int getImageResourceId() { return mImageResourceId; }
+
+    public int getAudioResourceId() {return mAudioResourceId; }
 
     public boolean hasImage() { return mImageResourceId != NO_IMAGE_PROVIDED; }
 }
