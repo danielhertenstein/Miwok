87946aa357b1ec42a58f7d213501023052e9be42
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2017-10-02 11:41:52 -0400

    Fixes colors

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +19,11 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+    private int mColorResourceId;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
         super(context, 0, words);
+        mColorResourceId = colorResourceId;
     }
 
     @NonNull
@@ -45,13 +49,17 @@ public class WordAdapter extends ArrayAdapter<Word> {
             imageView.setVisibility(View.GONE);
         }
 
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+
         // Find the TextView in the list_item.xml for the miwok translation
         TextView miwokTextView = listItemView.findViewById(R.id.miwok_text_view);
         miwokTextView.setText(currentWord.getMiwokTranslation());
+        miwokTextView.setBackgroundColor(color);
 
         // Find the TextView in the list_item.xml for the default translation
         TextView defaultTextView = listItemView.findViewById(R.id.default_text_view);
         defaultTextView.setText(currentWord.getDefaultTranslation());
+        defaultTextView.setBackgroundColor(color);
 
         return listItemView;
     }
